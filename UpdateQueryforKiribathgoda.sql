7f7b4612033977c03a0e2f1693dfe24dc210a642
chamarasanj <chamarasanj@gmail.com>
2020-12-29 16:17:28 +0530

    my first commit

---
+++ UpdateQueryforKiribathgoda.sql
@@ -0,0 +1,94 @@
+update [dbo].[Tbl_Mas_ItemDetail]
+set [Quantity]=52848
+where LocationId=1008 and ItemId=2056  --MA01
+
+select * from [dbo].[Tbl_Mas_ItemDetail] where LocationId=1008 and ItemId=2056
+
+
+update [dbo].[Tbl_Mas_ItemDetail]
+set [Quantity]=119
+where LocationId=1008 and ItemId=2063  --IN05
+
+select * from [dbo].[Tbl_Mas_ItemDetail] where LocationId=1008 and ItemId=2063
+
+
+update [dbo].[Tbl_Mas_ItemDetail]
+set [Quantity]=129
+where LocationId=1008 and ItemId=2064  --IN04
+
+select * from [dbo].[Tbl_Mas_ItemDetail] where LocationId=1008 and ItemId=2064
+
+
+
+update [dbo].[Tbl_Mas_ItemDetail]
+set [Quantity]=3
+where LocationId=1008 and ItemId=2065  --IN06
+
+select * from [dbo].[Tbl_Mas_ItemDetail] where LocationId=1008 and ItemId=2065
+
+
+
+update [dbo].[Tbl_Mas_ItemDetail]
+set [Quantity]=1
+where LocationId=1008 and ItemId=3053  --INC07
+
+select * from [dbo].[Tbl_Mas_ItemDetail] where LocationId=1008 and ItemId=3053
+
+
+
+update [dbo].[Tbl_Mas_ItemDetail]
+set [Quantity]=456
+where LocationId=1008 and ItemId=2070  --IN11
+
+select * from [dbo].[Tbl_Mas_ItemDetail] where LocationId=1008 and ItemId=2070
+
+
+update [dbo].[Tbl_Mas_ItemDetail]
+set [Quantity]=360
+where LocationId=1008 and ItemId=2071  --IN12
+
+select * from [dbo].[Tbl_Mas_ItemDetail] where LocationId=1008 and ItemId=2071
+
+
+update [dbo].[Tbl_Mas_ItemDetail]
+set [Quantity]=306
+where LocationId=1008 and ItemId=4056  --TP01
+
+select * from [dbo].[Tbl_Mas_ItemDetail] where LocationId=1008 and ItemId=4056
+
+
+update [dbo].[Tbl_Mas_ItemDetail]
+set [Quantity]=170
+where LocationId=1008 and ItemId=4057  --CN01
+
+select * from [dbo].[Tbl_Mas_ItemDetail] where LocationId=1008 and ItemId=4057
+
+
+update [dbo].[Tbl_Mas_ItemDetail]
+set [Quantity]=110
+where LocationId=1008 and ItemId=4058  --CN02
+
+select * from [dbo].[Tbl_Mas_ItemDetail] where LocationId=1008 and ItemId=4058
+
+
+update [dbo].[Tbl_Mas_ItemDetail]
+set [Quantity]=3
+where LocationId=1008 and ItemId=4064  --DI03
+
+select * from [dbo].[Tbl_Mas_ItemDetail] where LocationId=1008 and ItemId=4064
+
+
+
+
+update [dbo].[Tbl_Mas_ItemDetail]
+set [Quantity]=7
+where LocationId=1008 and ItemId=4065  --DI04
+
+select * from [dbo].[Tbl_Mas_ItemDetail] where LocationId=1008 and ItemId=4065
+
+
+
+
+select * 
+INTO Tbl_Mas_ItemDetail_Backup_05_dec_2020
+from [dbo].[Tbl_Mas_ItemDetail]
